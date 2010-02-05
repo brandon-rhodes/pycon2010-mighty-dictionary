97bd6a6cf1e179fa85eaef2506b86f5bd2bb32c0
Brandon Craig Rhodes <brandon@rhodesmill.org>
2010-02-04 22:24:48 -0500

    Adjusted slides to be exactly 800x600 pixels.

--- figures/average_probes.py
+++ figures/average_probes.py
@@ -8,7 +8,7 @@ for line in open('figures/average_probes_data.txt'):
     ya1.append(float(y1))
     ya2.append(float(y2))
 
-plt.figure(figsize=(6, 4))
+plt.figure(figsize=(8, 6))
 plt.plot(xa, ya1, 'r', lw=3)
 plt.text(1300, 2.5, 'average', color='r', fontsize=18)
 
@@ -18,5 +18,5 @@ plt.text(1300, 9, 'worst', color='c', fontsize=18)
 plt.plot([0, 3000], [1, 1], 'k--', lw=1)
 plt.plot([0, 3000], [2, 2], 'k--', lw=1)
 plt.axis([0, 3000, 0, 20])
-plt.ylabel('Probes per getitem', fontsize=18)
-plt.savefig(sys.argv[1], dpi=90)
+plt.ylabel('Probes per getitem', fontsize=16)
+plt.savefig(sys.argv[1], dpi=96)
