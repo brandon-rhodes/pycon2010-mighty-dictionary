a491a7e5bea6bb553d2316d282a5a1eaea5e5471
Brandon Craig Rhodes <brandon@rhodesmill.org>
2010-02-04 19:11:20 -0500

    Improved the size and fontsize of the figure.

--- figures/average_probes.py
+++ figures/average_probes.py
@@ -8,14 +8,15 @@ for line in open('figures/average_probes_data.txt'):
     ya1.append(float(y1))
     ya2.append(float(y2))
 
+plt.figure(figsize=(6, 4))
 plt.plot(xa, ya1, 'r', lw=3)
-plt.text(1300, 2.5, 'average', color='r', fontsize=30)
+plt.text(1300, 2.5, 'average', color='r', fontsize=18)
 
 plt.plot(xa, ya2, 'c', lw=3)
-plt.text(1300, 9, 'worst', color='c', fontsize=30)
+plt.text(1300, 9, 'worst', color='c', fontsize=18)
 
 plt.plot([0, 3000], [1, 1], 'k--', lw=1)
 plt.plot([0, 3000], [2, 2], 'k--', lw=1)
 plt.axis([0, 3000, 0, 20])
-plt.ylabel('Probes per getitem', fontsize=24)
-plt.savefig(sys.argv[1], dpi=60)
+plt.ylabel('Probes per getitem', fontsize=18)
+plt.savefig(sys.argv[1], dpi=90)
