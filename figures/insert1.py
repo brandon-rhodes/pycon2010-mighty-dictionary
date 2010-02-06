5851621df44530e10c34d39b2ec33ad41aaac177
Brandon Craig Rhodes <brandon@rhodesmill.org>
2010-02-05 19:13:33 -0500

    Got size-8 dictionaries looking okay in the diagram.

--- figures/insert1.py
+++ figures/insert1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+from math import ceil
 import cairo, sys
 import my_inspect
 
@@ -12,7 +13,7 @@ def bits(n, width=None):
     s = bin(n)[2:]  # '0b...' without the '0b'
     if width:
         s = '0' * (width - len(s)) + s
-    return s
+    return unicode(s)
 
 @contextmanager
 def save(cr):
@@ -33,41 +34,47 @@ cr.select_font_face('Inconsolata',
                     cairo.FONT_SLANT_NORMAL,
                     cairo.FONT_WEIGHT_BOLD)
 
-def draw_textbox(x, y, texts, rectpat):
+def draw_textbox(texts, rectcolor):
+
     with save(cr):
-        cr.translate(x, y)
         colors = [ a for i, a in enumerate(texts) if i%2 == 0 ]
         texts = [ a for i, a in enumerate(texts) if i%2 == 1 ]
-        fulltext = 'M' * sum( len(text) for text in texts )
-        tx, ty, twidth, theight = cr.text_extents(fulltext)[:4]
-        charwidth = twidth / len(fulltext)
-        padding = charwidth / 3
+
+        x, y = cr.get_current_point()
+        cr.translate(x, y)
+
+        extents = cr.text_extents('M' * len(''.join(texts)))
+        ty = ceil(extents[1])
+        twidth = ceil(extents[2])
+        theight = ceil(extents[3])
+
+        padding = ceil(theight / 3)
 
         width = twidth + 2 * padding
         height = theight + 2 * padding
         cr.rectangle(0,0, width,height)
-        cr.set_source(rectpat)
+        cr.set_source_rgb(*rectcolor)
         cr.fill()
 
-        cr.move_to(padding, padding + -ty)
+        cr.move_to(padding, padding + -ceil(ty))
         for i in range(len(colors)):
             cr.set_source_rgb(*colors[i])
+            if texts[i] == '/':  # special code for not-equals
+                with save(cr):   # so the '/' will display atop the '='
+                    cr.show_text('=')
             cr.show_text(texts[i])
 
-        return height
+    cr.rel_move_to(width, 0)
+    return height
 
-        cr.move_to(font_size / 5, slot_height / 2 + font_size / 3)
-        cr.set_source_rgb(1, 0.9, 0.5)
-        s = bin(i)[2:]
-        s = '0' * (3 - len(s)) + s
-        p = cr.get_current_point()
-        cr.show_text(s + '=' + '…')
-        cr.move_to(*p)
-        cr.show_text('   /')
-
+#
 
 white = (1, 1, 1)
+red = (0.7, 0, 0)
+green = (0, 0.7, 0)
 gold = (1, 0.9, 0.5)
+gray = (0.5, 0.5, 0.5)
+lightgray = (0.8, 0.8, 0.8)
 
 def draw_dictionary(d, x0, y0):
     """Supply `d` a Python dictionary."""
@@ -75,12 +82,12 @@ def draw_dictionary(d, x0, y0):
 
     with save(cr):
         if len(o) == 8:
-            font_size = 32
+            font_size = 28
             slot_height = 40
-            gap_height = 4
+            gap = 2
 
         cr.set_font_size(font_size)
-        charwidth = cr.text_extents('M')[2]
+        charwidth = cr.text_extents(u'M')[2]
         width = 100 #actually compute from font size later
 
         cr.translate(x0, slot_height)  # upper-left corner of the dictionary
@@ -88,46 +95,66 @@ def draw_dictionary(d, x0, y0):
         with save(cr):
             cr.set_source_rgb(0,0,0)
             cr.translate(2,-2)
-            cr.show_text('Hash')
+            cr.show_text(u'Idx    Hash')
 
         height = 0
 
         for i in range(len(o)):
-
-            cr.translate(0, height + gap_height)
+            cr.rel_move_to(0, height + gap)
 
             with save(cr):
-                texts = [white, bits(i, 3)]
-                height = draw_textbox(0, 0, texts, pat)
-                continue
-                cr.rectangle(0,0, width,slot_height)
-                cr.set_source(pat)
-                cr.fill()
+                entry = o.ma_table[i]
 
-                cr.move_to(font_size / 5, slot_height / 2 + font_size / 3)
-                cr.set_source_rgb
-                p = cr.get_current_point()
-                print cr.text_extents(s + '=')
-                cr.show_text(s + '=' + '…')
-                cr.move_to(*p)
-                cr.show_text('   /')
-            #try:
-            #    cr.show_text(' ' + k)
-            #except ValueError:
-            #    cr.show_text(' ' + k)
+                height = draw_textbox([gold, bits(i, 3)], gray)
+                cr.rel_move_to(gap, 0)
 
-                #try:
-                #    v = o.ma_table[i].me_key
-                #except ValueError:
-                #    v = 'NULL'
-                
-                #cr.show_text(' ' + v)
+                try:
+                    k = entry.me_key
+                except ValueError:
+                    # This is a completely empty entry.
+                    draw_textbox([white, u' '], lightgray)
+                    cr.rel_move_to(gap, 0)
+                    draw_textbox([white, u' ' * 9], lightgray)
+                    cr.rel_move_to(gap, 0)
+                    draw_textbox([white, u' ' * 7], lightgray)
+                    cr.rel_move_to(gap, 0)
+                    draw_textbox([white, u' ' * 7], lightgray)
+                    continue
+
+                if k is my_inspect.dummy:
+                    draw_textbox([white, u'!'], red)
+                    cr.rel_move_to(gap, 0)
+                    draw_textbox([white, u' ' * 9], gray)
+                    cr.rel_move_to(gap, 0)
+                    draw_textbox([white, u'<dummy>'], gray)
+                    cr.rel_move_to(gap, 0)
+                    draw_textbox([white, u' ' * 7], gray)
+                    continue
+
+                h = entry.me_hash
+                v = entry.me_value
+
+                if h & o.ma_mask == i:
+                    draw_textbox([white, u'='], green)
+                else:
+                    draw_textbox([white, u'/'], red)
+                cr.rel_move_to(gap, 0)
+                bstr = bits(h, 8)[:8]
+                texts = [lightgray, u'…' + bstr[:-3], gold, bstr[-3:]]
+                draw_textbox(texts, gray)
+                cr.rel_move_to(gap, 0)
+                draw_textbox([white, u'%7s' % repr(k)], gray)
+                cr.rel_move_to(gap, 0)
+                draw_textbox([white, u'%7s' % repr(v)], gray)
 
 #
 
 cr.rectangle(0,0, WIDTH,HEIGHT)
 cr.set_source_rgb(1,1,1)
 cr.fill()
-draw_dictionary({}, 100, 20)
+d = {'a': 1, 'b': 2, 'i': 3}
+del d['a']
+
+draw_dictionary(d, 100, 20)
 
 surface.write_to_png(sys.argv[1])
